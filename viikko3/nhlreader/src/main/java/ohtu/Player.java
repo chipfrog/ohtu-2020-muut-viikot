9434d7729a9e81979473c0ba49b26259a2b0f069
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2020-03-24 22:48:15 +0200

    Tehtava 1

--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -3,6 +3,12 @@ package ohtu;
 
 public class Player {
     private String name;
+    private String team;
+    private String nationality;
+    private String birthday;
+    private int goals;
+    private int assists;
+    private int penalties;
 
     public void setName(String name) {
         this.name = name;
@@ -12,9 +18,13 @@ public class Player {
         return name;
     }
 
+    public String getNationality() {
+        return nationality;
+    }
+
     @Override
     public String toString() {
-        return name;
+        return name + " team " + team + " goals " + goals + " assists " + assists;
     }
       
 }
