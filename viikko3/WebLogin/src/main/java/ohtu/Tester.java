aec5d94e7a8f2b867a1c316da00cabb16e0e65c7
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2020-03-26 00:02:56 +0200

    Muokattu Testerin usernamejen satunnaisgenerointia

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -11,11 +11,20 @@ public class Tester {
 
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+
+        // Luodaan satunnaisia aakkosia uniikkien usernamejen luomista varten, koska myöhemmässä tehtävänannossakin määriteltiin, että
+        // usernamet saavat sisältää vain merkkejä a-z, joten numerot eivät kelpaa.
         Random r = new Random();
+        char k1 = (char) (r.nextInt(26) + 'a');
+        char k2 = (char) (r.nextInt(26) + 'a');
+        char k3 = (char) (r.nextInt(26) + 'a');
+        char k4 = (char) (r.nextInt(26) + 'a');
+        char k5 = (char) (r.nextInt(26) + 'a');
 
         driver.get("http://localhost:4567");
         
         // Alkuperäinen skenaario
+
         /*sleep(2);
         
         WebElement element = driver.findElement(By.linkText("login"));
@@ -53,29 +62,7 @@ public class Tester {
 
         sleep(3);*/
 
-        // Uuden käyttäjätunnukusen luominen
-
-        /*sleep(2);
-        WebElement element = driver.findElement(By.linkText("register new user"));
-        element.click();
-
-        sleep(2);
-
-        element = driver.findElement((By.name("username")));
-        element.sendKeys("uusi" + r.nextInt(100000));
-        element = driver.findElement(By.name("password"));
-        element.sendKeys("salasana123");
-        element = driver.findElement(By.name("passwordConfirmation"));
-        element.sendKeys("salasana123");
-        element = driver.findElement(By.name("signup"));
-
-        sleep(2);
-        element.submit();
-
-        sleep(3);*/
-
-
-        // Uuden käyttäjän luominen + uloskirjautuminen
+        // Uuden käyttäjätunnuksen luominen
 
         sleep(2);
         WebElement element = driver.findElement(By.linkText("register new user"));
@@ -84,7 +71,7 @@ public class Tester {
         sleep(2);
 
         element = driver.findElement((By.name("username")));
-        element.sendKeys("uusi" + r.nextInt(100000));
+        element.sendKeys("uusi" + k1 + k2 + k3 + k4 + k5);
         element = driver.findElement(By.name("password"));
         element.sendKeys("salasana123");
         element = driver.findElement(By.name("passwordConfirmation"));
@@ -95,6 +82,8 @@ public class Tester {
         element.submit();
 
         sleep(2);
+
+        // Uloskirjautuminen käyttäjän luomisen jälkeen
 
         element = driver.findElement(By.linkText("continue to application mainpage"));
         element.click();
@@ -107,7 +96,6 @@ public class Tester {
         sleep(2);
 
         // Ohjelman lopetus
-
 
         driver.quit();
     }
